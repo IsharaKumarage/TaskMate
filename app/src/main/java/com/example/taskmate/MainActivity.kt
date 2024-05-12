b49ff4f8ef1f2baea59f63faf0da02ee2fe6506f
isha_kumarage <isharakumarage027@gmail.com>
2024-05-12 19:00:50 +0530

    Implement Main Activity

--- app/src/main/java/com/example/taskmate/MainActivity.kt
+++ app/src/main/java/com/example/taskmate/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.taskmate
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.taskmate.databinding.ActivityMainBinding // Import your binding class here
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // Declare your binding variable here
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
         }
     }
 }
